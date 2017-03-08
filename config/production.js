2fbfd7b9ccab1338729d2519a9dfd330eda12e3d
xianfei689 <347560189@qq.com>
2017-03-08 16:59:23 +0800

    @xianfei add

--- config/production.js
+++ config/production.js
@@ -1,18 +1,18 @@
-module.exports = {
-  port: 3000,
-  session: {
-    secret: 'myblog',
-    key: 'myblog',
-    maxAge: 2592000000
-  },
-  mongodb: 'mongodb://localhost:27017/myblog'
-};
 // module.exports = {
-//   port: 80,
-//    session: {
-//      secret: 'myblog',
-//      key: 'myblog',
-//      maxAge: 2592000000
-//    },
-//   mongodb: 'mongodb://myblog:myblog@ds145289.mlab.com:45289/myblog'
+//   port: 3000,
+//   session: {
+//     secret: 'myblog',
+//     key: 'myblog',
+//     maxAge: 2592000000
+//   },
+//   mongodb: 'mongodb://localhost:27017/myblog'
 // };
+module.exports = {
+  port: 80,
+   session: {
+     secret: 'myblog',
+     key: 'myblog',
+     maxAge: 2592000000
+   },
+  mongodb: 'mongodb://myblog:myblog@ds145289.mlab.com:45289/myblog'
+};
